b0f707890b9fbe18f4d422071dafd82bb2f1f881
Administrator <Administrator@smart20116>
2017-08-10 10:47:38 +0900

    일반회원가입 폼과 액션 안올린 xml파일 올림 테스트02

---
+++ harulook/src/main/java/com/ksmart/harulook/dto/MemberDto.java
@@ -0,0 +1,169 @@
+package com.ksmart.harulook.dto;
+
+public class MemberDto {
+	private String user_id;
+	private String user_pw;
+	private String user_nick;
+	private String user_name;
+	private String user_tel;
+	private String user_email;
+	private String user_addr;
+	private String user_gender;
+	private String user_age;
+	private String user_tall;
+	private String user_size;
+	private String user_q;
+	private String user_a;
+	private String user_level;
+	private String user_bs_no;
+	private String user_bs_type;
+	private String user_bs_status;
+	private String user_bs_name;
+	private String user_url;
+	private int user_point;
+	private String user_date;
+	
+	
+	@Override
+	public String toString() {
+		return "MemberDto [user_id=" + user_id + ", user_pw=" + user_pw + ", user_nick=" + user_nick + ", user_name="
+				+ user_name + ", user_tel=" + user_tel + ", user_email=" + user_email + ", user_addr=" + user_addr
+				+ ", user_gender=" + user_gender + ", user_age=" + user_age + ", user_tall=" + user_tall
+				+ ", user_size=" + user_size + ", user_q=" + user_q + ", user_a=" + user_a + ", user_level="
+				+ user_level + ", user_bs_no=" + user_bs_no + ", user_bs_type=" + user_bs_type + ", user_bs_status="
+				+ user_bs_status + ", user_bs_name=" + user_bs_name + ", user_url=" + user_url + ", user_point="
+				+ user_point + ", user_date=" + user_date + "]";
+	}
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pw() {
+		return user_pw;
+	}
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	}
+	public String getUser_nick() {
+		return user_nick;
+	}
+	public void setUser_nick(String user_nick) {
+		this.user_nick = user_nick;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_tel() {
+		return user_tel;
+	}
+	public void setUser_tel(String user_tel) {
+		this.user_tel = user_tel;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_addr() {
+		return user_addr;
+	}
+	public void setUser_addr(String user_addr) {
+		this.user_addr = user_addr;
+	}
+	public String getUser_gender() {
+		return user_gender;
+	}
+	public void setUser_gender(String user_gender) {
+		this.user_gender = user_gender;
+	}
+	public String getUser_age() {
+		return user_age;
+	}
+	public void setUser_age(String user_age) {
+		this.user_age = user_age;
+	}
+	public String getUser_tall() {
+		return user_tall;
+	}
+	public void setUser_tall(String user_tall) {
+		this.user_tall = user_tall;
+	}
+	public String getUser_size() {
+		return user_size;
+	}
+	public void setUser_size(String user_size) {
+		this.user_size = user_size;
+	}
+	public String getUser_q() {
+		return user_q;
+	}
+	public void setUser_q(String user_q) {
+		this.user_q = user_q;
+	}
+	public String getUser_a() {
+		return user_a;
+	}
+	public void setUser_a(String user_a) {
+		this.user_a = user_a;
+	}
+	public String getUser_level() {
+		return user_level;
+	}
+	public void setUser_level(String user_level) {
+		this.user_level = user_level;
+	}
+	public String getUser_bs_no() {
+		return user_bs_no;
+	}
+	public void setUser_bs_no(String user_bs_no) {
+		this.user_bs_no = user_bs_no;
+	}
+	public String getUser_bs_type() {
+		return user_bs_type;
+	}
+	public void setUser_bs_type(String user_bs_type) {
+		this.user_bs_type = user_bs_type;
+	}
+	public String getUser_bs_status() {
+		return user_bs_status;
+	}
+	public void setUser_bs_status(String user_bs_status) {
+		this.user_bs_status = user_bs_status;
+	}
+	public String getUser_bs_name() {
+		return user_bs_name;
+	}
+	public void setUser_bs_name(String user_bs_name) {
+		this.user_bs_name = user_bs_name;
+	}
+	public String getUser_url() {
+		return user_url;
+	}
+	public void setUser_url(String user_url) {
+		this.user_url = user_url;
+	}
+	public int getUser_point() {
+		return user_point;
+	}
+	public void setUser_point(int user_point) {
+		this.user_point = user_point;
+	}
+	public String getUser_date() {
+		return user_date;
+	}
+	public void setUser_date(String user_date) {
+		this.user_date = user_date;
+	}
+	
+	
+	
+	
+	
+}
