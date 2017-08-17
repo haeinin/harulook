5eef69c504d82ce326dc1c2483e83c6e002a0f26
injun379 <acddde9@naver.com>
2017-08-17 10:07:41 +0900

    java source 폴더 구조 변경

--- harulook/src/main/java/com/ksmart/harulook/like/controller/LikeController.java
+++ harulook/src/main/java/com/ksmart/harulook/like/controller/LikeController.java
@@ -1,5 +1,22 @@
 package com.ksmart.harulook.like.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ksmart.harulook.like.service.LikeDao;
+import com.ksmart.harulook.like.service.LikeDto;
+
+@Controller
 public class LikeController {
 
+	@Autowired
+	private LikeDao likeDao;
+	
+	@RequestMapping(value="/likeInsert", method = RequestMethod.GET)
+	public String likeInsert(LikeDto like) {
+		
+		return "";
+	}
 }
--- harulook/src/main/java/com/ksmart/harulook/like/service/LikeDao.java
+++ harulook/src/main/java/com/ksmart/harulook/like/service/LikeDao.java
@@ -1,5 +1,21 @@
 package com.ksmart.harulook.like.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LikeDao {
 
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public int likeInsert(LikeDto like) {
+		return sqlSessionTemplate.insert("com.ksmart.harulook.like.LikeMapper.likeInsert", like);
+	}
+	
+	public int likeDelete(LikeDto like) {
+		return sqlSessionTemplate.delete("com.ksmart.harulook.like.LikeMapper.likeDelete", like);
+	}
+	
 }
